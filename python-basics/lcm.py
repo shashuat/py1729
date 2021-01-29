dce85f42f955ad35c159e7c1c955e41c19a22911
zshashz <72076441+zshashz@users.noreply.github.com>
2021-01-29 19:55:15 +0530

    Create lcm.py

---
+++ python-basics/lcm.py
@@ -0,0 +1,20 @@
+def compute_lcm(x, y):
+
+   if x > y:
+       greater = x
+   else:
+       greater = y
+
+   while(True):
+       if((greater % x == 0) and (greater % y == 0)):
+           lcm = greater
+           break
+       greater += 1
+
+   return lcm
+
+num1 = int(input("num 1 "))
+num2 = int(input("num 2 "))
+
+
+print("The L.C.M. is", compute_lcm(num1, num2))
