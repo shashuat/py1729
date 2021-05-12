d40e77fd9e68cea9f49dc374e17a987346ab21b9
zshashz <zshashwatz@gmail.com>
2021-05-12 20:33:50 +0530

    Update periodicrickroll.py

--- projects/periodicrickroll.py
+++ projects/periodicrickroll.py
@@ -7,6 +7,6 @@ break_count=0
 print("This Program Started on " + time.ctime())
 
 while(break_count < total_breaks):
-    time.sleep(69)
+    time.sleep(5)
     webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
     break_count=break_count+1
