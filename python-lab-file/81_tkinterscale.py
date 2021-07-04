d48b8f78718b66ce1ffe75792c3261d8862b1e61
zshashz <72076441+zshashz@users.noreply.github.com>
2021-07-05 00:18:46 +0530

    Create 81_tkinterscale.py

---
+++ python-lab-file/81_tkinterscale.py
@@ -0,0 +1,9 @@
+# Program 81 : Make tkinter graphic slide/scale
+
+from tkinter import *
+master = Tk()
+w = Scale(master, from_=0, to=42)
+w.pack()
+w = Scale(master, from_=0, to=200, orient=HORIZONTAL)
+w.pack()
+mainloop()
